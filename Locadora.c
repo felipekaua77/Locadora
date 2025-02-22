0904f5bd78a461c30e78d30dff06584abfa36260
Felipe Kauã <felipekaua77@gmail.com>
2025-02-22 14:08:00 -0300

    Update Locadora.c

--- Locadora.c
+++ Locadora.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 
-int main(){
+void menu(){
     printf("BEM VINDO A LOCADORA DOS FÃS DE VALÉRIA\n");
     printf("Temos os melhores filmes da região!!!\n");
     printf("Aqui você encontra os melhores filmes de comédia, ação, romance e muito mais!!!\n");
+    printf("Escolha uma opção:\n");
+    printf("1.Terror\n");
+    printf("2.Comédia\n");
+    printf("3.Ação\n");
+    printf("4.Romance\n");
+    printf("5.Aventura\n");
+    
 
+    return 0;
+}
+
+
+
+int main(){
+    menu();
     return 0;
 }
