48cb686aff3034cf4d2c0764e9c2cba668d160b5
Tr4k1nasofficial <cxstudying018@gmail.com>
2025-02-22 14:29:58 -0300

    locadorav.0.1

--- Locadora.c
+++ Locadora.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 void menu(){
+    int op = 0;
     printf("BEM VINDO A LOCADORA DOS FÃS DE VALÉRIA\n");
     printf("Temos os melhores filmes da região!!!\n");
     printf("Aqui você encontra os melhores filmes de comédia, ação, romance e muito mais!!!\n");
@@ -10,11 +11,39 @@ void menu(){
     printf("3.Ação\n");
     printf("4.Romance\n");
     printf("5.Aventura\n");
-    
-
+    printf("Digite aqui: ");
+    scanf("%d", &op);
     return 0;
 }
 
+void menu_filme_terror(){
+    printf("1.Sexta feira 13\n");
+    printf("2.O Exorcista\n");
+    printf("3.Invocação do Mal\n");
+    printf("4.O Chamado\n");
+    printf("5.Hora do pesadelo\n");
+    printf("6.sair\n");
+    int opcao;
+    scanf("%d", &opcao);   
+    	
+}
+
+void menu_filme_comedia(){
+
+}
+
+void menu_filme_acao(){
+
+}
+
+void menu_filme_romance(){
+    printf("Infelizmente estamos sem filmes desta categoria :c\n");
+    pritnf("1.sair");
+}
+
+void menu_filme_aventura(){
+
+}
 
 
 int main(){
