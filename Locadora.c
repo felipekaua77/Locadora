cda957ce9fd8cadb3ef667d151548cda3d1ad4ab
Felipe Kauã <felipekaua77@gmail.com>
2025-02-24 08:58:11 -0300

    Elaboração da matriz

--- Locadora.c
+++ Locadora.c
@@ -1,75 +1,118 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 
 typedef struct cadastro_pessoa {
-    char *nome;
+    char nome[50];
     int id;
-    struct cad_filme_pessoa;
-}cadastro_pessoa;
+} cadastro_pessoa;
 
 typedef struct cadastro_filme {
-    char *nome_filme;
-    char *genero_filme;
+    char nome_filme[50];
+    char genero_filme[30];
     float preco_filme;
-}cadastro_filme;
+    int disponivel; // 1 para disponível, 0 para alugado
+} cadastro_filme;
 
-typedef struct agendametos{
+typedef struct agendamentos {
     int id;
     int id_filme;
     int id_pessoa;
     int data;
     int hora;
-}agendamentos;
+} agendamentos;
 
-void menu(){
-    int op = 0;
-    printf("BEM VINDO A LOCADORA DOS FÃS DE VALÉRIA\n");
-    printf("Temos os melhores filmes da região!!!\n");
-    printf("Aqui você encontra os melhores filmes de comédia, ação, romance e muito mais!!!\n");
-    printf("Escolha uma opção:\n");
-    printf("1.Terror\n");
-    printf("2.Comédia\n");
-    printf("3.Ação\n");
-    printf("4.Romance\n");
-    printf("5.Aventura\n");
-    printf("Digite aqui: ");
-    scanf("%d", &op);
-    return 0;
+cadastro_filme** criarTabuleiro(int linhas, int colunas) {
+    cadastro_filme** tabuleiro = (cadastro_filme**)malloc(linhas * sizeof(cadastro_filme*));
+    for (int i = 0; i < linhas; i++) {
+        tabuleiro[i] = (cadastro_filme*)malloc(colunas * sizeof(cadastro_filme));
+    }
+    return tabuleiro;
 }
 
-void menu_filme_terror(){
-    printf("1.Sexta feira 13\n");
-    printf("2.O Exorcista\n");
-    printf("3.Invocação do Mal\n");
-    printf("4.O Chamado\n");
-    printf("5.Hora do pesadelo\n");
-    printf("6.sair\n");
-    int opcao;
-    scanf("%d", &opcao);   
-    	
+void imprimirTabuleiro(cadastro_filme** tabuleiro, int linhas, int colunas) {
+    // Calcula o comprimento máximo dos nomes dos filmes
+    int maxLen = 1; // Para o caso de "X"
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            int len = strlen(tabuleiro[i][j].nome_filme);
+            if (len > maxLen) {
+                maxLen = len;
+            }
+        }
+    }
+
+    // Imprime os números das colunas
+    printf("    ");
+    for (int j = 0; j < colunas; j++) {
+        printf(" %*d ", maxLen, j);
+    }
+    printf("\n");
+
+    // Imprime o tabuleiro
+    for (int i = 0; i < linhas; i++) {
+        // Imprime linha de separação
+        printf("   ");
+        for (int j = 0; j < colunas; j++) { 
+            printf("+");
+            for (int k = 0; k < maxLen + 2; k++) {
+                printf("-");
+            }
+        }
+        printf("+\n");
+
+        // Imprime o número da linha
+        printf(" %2d ", i);
+
+        // Imprime os filmes
+        for (int j = 0; j < colunas; j++) { // Imprime "X" se o filme não estiver disponível
+            if (tabuleiro[i][j].disponivel == 0) {
+                printf("| %*s ", -maxLen, "X");
+            } else {
+                printf("| %*s ", -maxLen, tabuleiro[i][j].nome_filme);
+            }
+        }
+        printf("|\n");
+    }
+
+    // Imprime linha de separação final
+    printf("   ");
+    for (int j = 0; j < colunas; j++) {
+        printf("+");
+        for (int k = 0; k < maxLen + 2; k++) {
+            printf("-");
+        }
+    }
+    printf("+\n");
 }
 
-void menu_filme_comedia(){
-
-}
-
-void menu_filme_acao(){
-
-}
-
-void menu_filme_romance(){
-    printf("Infelizmente estamos sem filmes desta categoria :c\n");
-    pritnf("1.sair");
-}
-
-void menu_filme_aventura(){
-
-}
-
-
-int main(){
+int main() {
     srand(time(NULL));
-    menu();
+
+    int linhas = 15;
+    int colunas = 4;
+
+    cadastro_filme** tabuleiro = criarTabuleiro(linhas, colunas);
+
+    // Exemplo de preenchimento do tabuleiro
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            snprintf(tabuleiro[i][j].nome_filme, 50, "Filme%d%d", i, j);
+            snprintf(tabuleiro[i][j].genero_filme, 30, "Genero%d%d", i, j);
+            tabuleiro[i][j].preco_filme = (float)(rand() % 100) / 10.0;
+            tabuleiro[i][j].disponivel = 1; // Todos os filmes começam como disponíveis
+        }
+    }
+
+    printf("Tabuleiro de Filmes:\n");
+    imprimirTabuleiro(tabuleiro, linhas, colunas);
+
+    // Libera a memória alocada
+    for (int i = 0; i < linhas; i++) {
+        free(tabuleiro[i]);
+    }
+    free(tabuleiro);
+
     return 0;
 }
