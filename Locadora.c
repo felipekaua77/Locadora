c3c34ad859c3a1a4472ea05982e685f07d794ee8
FelipeCauan <felipecauansf@gmail.com>
2025-02-24 22:00:15 -0300

    Update Locadora.c

--- Locadora.c
+++ Locadora.c
@@ -4,19 +4,19 @@
 #include <time.h>
 
 #define MAX_GENEROS 5
-#define MAX_FILMES_POR_GENERO 15
+#define MAX_FILMES 15
 
-typedef struct {
+typedef struct filme{
     char nome_filme[100];
     float preco_filme;
     int disponivel; // 1 - Disponivel, 0 - Alugado
-} Filme;
+}Filme;
 
-typedef struct {
+typedef struct genero{
     char nome_genero[50];
-    Filme filmes[MAX_FILMES_POR_GENERO];
+    Filme filmes[MAX_FILMES];
     int num_filmes;
-} Genero;
+}Genero;
 
 Genero generos[MAX_GENEROS] = {
     {"Terror", {}, 0},
@@ -27,50 +27,57 @@ Genero generos[MAX_GENEROS] = {
 };
 
 // Função que compara dois filmes pelo nome para ordenação
-int comparar_filmes(const void *a, const void *b) {
+int comparar_filmes(const void *a, const void *b){
     Filme *filmeA = (Filme *)a;
     Filme *filmeB = (Filme *)b;
+
     return strcmp(filmeA->nome_filme, filmeB->nome_filme);
 }
 
 // Função que ordena os filmes de cada gênero em ordem alfabética
-void ordenar_filmes() {
-    for (int i = 0; i < MAX_GENEROS; i++) {
+void ordenar_filmes(){
+    for(int i = 0; i < MAX_GENEROS; i++){
         qsort(generos[i].filmes, generos[i].num_filmes, sizeof(Filme), comparar_filmes);
     }
 }
 
 // Função que cadastra um novo filme em um gênero escolhido pelo usuário
-void cadastrar_filme() {
+void cadastrar_filme(){
     int genero_id;
-    printf("Escolha o genero do filme:\n");
-    for (int i = 0; i < MAX_GENEROS; i++) {
+
+    printf("\nEscolha o genero do filme:\n");
+    for(int i = 0; i < MAX_GENEROS; i++){
         printf("%d - %s\n", i + 1, generos[i].nome_genero);
     }
-    printf("Digite a opcao desejada: ");
+    printf("\nDigite a opcao desejada:\n> ");
     scanf("%d", &genero_id);
     genero_id--;
 
-    if (genero_id >= 0 && genero_id < MAX_GENEROS && generos[genero_id].num_filmes < MAX_FILMES_POR_GENERO) {
+    if(genero_id >= 0 && genero_id < MAX_GENEROS && generos[genero_id].num_filmes < MAX_FILMES){
         Filme *novo_filme = &generos[genero_id].filmes[generos[genero_id].num_filmes];
-        printf("Digite o nome do filme: ");
+
+        printf("\nDigite o nome do filme: ");
         scanf(" %[^\n]s", novo_filme->nome_filme);
         printf("Digite o preco do filme: ");
         scanf("%f", &novo_filme->preco_filme);
         novo_filme->disponivel = 1;
         generos[genero_id].num_filmes++;
+
         ordenar_filmes(); // Ordena os filmes após cadastrar um novo filme
-        printf("\n\nFilme cadastrado com sucesso!\n");
-    } else {
-        printf("Genero invalido ou limite de filmes atingido!\n");
+        printf("\nFilme cadastrado com sucesso!\n");
+    } else{
+        printf("\nGenero invalido ou limite de filmes atingido!\n");
     }
 }
 
 // Função que lista todos os filmes cadastrados, organizados por gênero
-void listar_filmes() {
-    for (int i = 0; i < MAX_GENEROS; i++) {
+void listar_filmes(){
+    printf("\n");
+
+    for(int i = 0; i < MAX_GENEROS; i++){
         printf("Genero: %s\n", generos[i].nome_genero);
-        for (int j = 0; j < generos[i].num_filmes; j++) {
+
+        for(int j = 0; j < generos[i].num_filmes; j++){
             Filme *filme = &generos[i].filmes[j];
             printf("  Filme %d: %s\n", j + 1, filme->nome_filme);
             printf("    Preco: %.2f\n", filme->preco_filme);
@@ -80,79 +87,85 @@ void listar_filmes() {
 }
 
 // Função que remove um filme de um gênero escolhido pelo usuário
-void remover_filme() {
+void remover_filme(){
     int genero_id, filme_id;
-    printf("Escolha o genero do filme a ser removido:\n");
-    for (int i = 0; i < MAX_GENEROS; i++) {
+
+    printf("\nEscolha o genero do filme a ser removido:\n");
+    for(int i = 0; i < MAX_GENEROS; i++){
         printf("%d - %s\n", i + 1, generos[i].nome_genero);
     }
-    printf("Digite a opcao desejada: ");
+    printf("\nDigite a opcao desejada:\n> ");
     scanf("%d", &genero_id);
     genero_id--;
 
-    if (genero_id >= 0 && genero_id < MAX_GENEROS && generos[genero_id].num_filmes > 0) {
-        printf("Escolha o filme a ser removido:\n");
-        for (int j = 0; j < generos[genero_id].num_filmes; j++) {
+    if(genero_id >= 0 && genero_id < MAX_GENEROS && generos[genero_id].num_filmes > 0){
+        printf("\nEscolha o filme a ser removido:\n");
+
+        for(int j = 0; j < generos[genero_id].num_filmes; j++){
             printf("%d - %s\n", j + 1, generos[genero_id].filmes[j].nome_filme);
         }
-        printf("Digite a opcao desejada: ");
+        printf("\nDigite a opcao desejada:\n> ");
         scanf("%d", &filme_id);
         filme_id--;
 
-        if (filme_id >= 0 && filme_id < generos[genero_id].num_filmes) {
-            for (int j = filme_id; j < generos[genero_id].num_filmes - 1; j++) {
+        if(filme_id >= 0 && filme_id < generos[genero_id].num_filmes){
+            for(int j = filme_id; j < generos[genero_id].num_filmes - 1; j++){
                 generos[genero_id].filmes[j] = generos[genero_id].filmes[j + 1];
             }
             generos[genero_id].num_filmes--;
-            printf("\n\nFilme removido com sucesso!\n");
-        } else {
-            printf("Filme invalido!\n");
+            printf("\nFilme removido com sucesso!\n");
+        } else{
+            printf("\nFilme invalido!\n");
         }
-    } else {
-        printf("Genero invalido ou nenhum filme cadastrado nesse genero!\n");
+    } else{
+        printf("\nGenero invalido ou nenhum filme cadastrado nesse genero!\n");
     }
 }
 
 // Função que edita as informações de um filme já cadastrado
-void editar_filme() {
+void editar_filme(){
     int genero_id, filme_id;
-    printf("Escolha o genero do filme a ser editado:\n");
-    for (int i = 0; i < MAX_GENEROS; i++) {
+
+    printf("\nEscolha o genero do filme a ser editado:\n");
+    for(int i = 0; i < MAX_GENEROS; i++){
         printf("%d - %s\n", i + 1, generos[i].nome_genero);
     }
-    printf("Digite a opcao desejada: ");
+    printf("\nDigite a opcao desejada:\n> ");
     scanf("%d", &genero_id);
     genero_id--;
 
-    if (genero_id >= 0 && genero_id < MAX_GENEROS && generos[genero_id].num_filmes > 0) {
-        printf("Escolha o filme a ser editado:\n");
-        for (int j = 0; j < generos[genero_id].num_filmes; j++) {
+    if(genero_id >= 0 && genero_id < MAX_GENEROS && generos[genero_id].num_filmes > 0){
+        printf("\nEscolha o filme a ser editado:\n");
+
+        for(int j = 0; j < generos[genero_id].num_filmes; j++){
             printf("%d - %s\n", j + 1, generos[genero_id].filmes[j].nome_filme);
         }
-        printf("Digite a opcao desejada: ");
+        printf("\nDigite a opcao desejada:\n> ");
         scanf("%d", &filme_id);
         filme_id--;
 
-        if (filme_id >= 0 && filme_id < generos[genero_id].num_filmes) {
+        if(filme_id >= 0 && filme_id < generos[genero_id].num_filmes){
             Filme *filme = &generos[genero_id].filmes[filme_id];
-            printf("Digite o novo nome do filme: ");
+
+            printf("\nDigite o novo nome do filme: ");
             scanf(" %[^\n]s", filme->nome_filme);
             printf("Digite o novo preco do filme: ");
             scanf("%f", &filme->preco_filme);
-            printf("O filme está disponível? (1 - Sim, 0 - Não): ");
+            printf("O filme esta disponivel? (1 - Sim, 0 - Nao): ");
             scanf("%d", &filme->disponivel);
+
             ordenar_filmes(); // Ordena os filmes após editar um filme
-            printf("\n\nFilme editado com sucesso!\n");
+            printf("\nFilme editado com sucesso!\n");
         } else {
-            printf("Filme invalido!\n");
+            printf("\nFilme invalido!\n");
         }
     } else {
-        printf("Genero invalido ou nenhum filme cadastrado nesse genero!\n");
+        printf("\nGenero invalido ou nenhum filme cadastrado nesse genero!\n");
     }
 }
 
 // Função que exibe o menu principal e gerencia as opções escolhidas pelo usuário
-void menu() {
+void menu_filmes() {
     int op = -1;
 
     while (op != 0) {
@@ -186,7 +199,7 @@ void menu() {
 // Função principal que inicializa o programa
 int main() {
     srand(time(NULL));
-    menu();
+    menu_filmes();
 
     return 0;
 }
