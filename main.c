44a1d180dd5aeae438ac042f6f6864ee74af7e13
FelipeCauan <felipecauansf@gmail.com>
2025-02-27 10:09:02 -0300

    summary required

--- main.c
+++ main.c
@@ -6,11 +6,14 @@ void menu_principal(){
     int opcao = -1;
 
     while(opcao!=0){
-        printf("\n////////// Menu Principal //////////\n\n");
+        printf("\n////////// MENU PRINCIPAL //////////\n");
+        printf("BEM-VINDO A FILMES, FILMES E MAX FILMES!\n");
+        printf("QUEREMOS MAIS, MAIS E MAIS FILMES!\n\n");
         printf("1 - Gerenciar Filmes\n");
         printf("2 - Gerenciar Clientes\n");
         printf("3 - Gerenciar Alugueis\n");
         printf("0 - Encerrar o programa\n");
+        printf("\n////////////////////////////////////\n");
         printf("\nDigite a opcao desejada:\n> ");
         scanf("%d", &opcao);
 
